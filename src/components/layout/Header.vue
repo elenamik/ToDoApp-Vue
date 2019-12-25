c1ed41bbe6144c9e1614e953961c67d2a3b8d606
Elena Mikhaylova <Lena@Mac.lan>
2019-12-25 17:34:12 -0500

    to do app

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <h1>To Do List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background-color: rgb(75, 11, 11);
+    color:rgb(255, 255, 235);
+    padding: 10px;
+    text-align: center;
+  }
+</style> 
